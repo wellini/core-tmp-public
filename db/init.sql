27b55358d9b59826dc418e08f30bdc7074a3639f
winterfir <weirdogeddon06@gmail.com>
2021-01-09 07:28:49 +0300

    fix core for pipeline

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,6 @@
 CREATE DATABASE roadmaps_db;
 CREATE ROLE roadmaps WITH LOGIN roadmaps PASSWORD 'roadmaps';
+CREATE SCHEMA roadmaps AUTHORIZATION roadmaps;
+grant create on schema roadmaps to roadmaps;
+grant usage on schema roadmaps to roadmaps;
+SET search_path TO roadmaps,public;
