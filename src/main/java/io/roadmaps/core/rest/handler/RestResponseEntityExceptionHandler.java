f81e6042532b2fa15eb0029c2a290d4ec9e5d077
wellini <40068963+wellini@users.noreply.github.com>
2021-05-03 16:06:09 +0300

    Fix statuses (#8)
    Co-authored-by: Alexander <isanny314@gmail.com>

--- src/main/java/io/roadmaps/core/rest/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/io/roadmaps/core/rest/handler/RestResponseEntityExceptionHandler.java
@@ -3,25 +3,29 @@ package io.roadmaps.core.rest.handler;
 import io.roadmaps.core.exception.RoadmapsException;
 import io.roadmaps.core.rest.common.dto.ApiErrorDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static org.springframework.http.ResponseEntity.ok;
+import java.util.Optional;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {RoadmapsException.class})
     public ResponseEntity<ApiErrorDto> handleManagedException(RoadmapsException ex, WebRequest request) {
         log.error("RestResponseEntityExceptionHandler", ex);
-        return ok(new ApiErrorDto(ex.getExplanation()));
+        ApiErrorDto dto = new ApiErrorDto(ex.getExplanation());
+        HttpStatus status = getHttpStatus(ex);
+        return new ResponseEntity<>(dto, status);
     }
 
     @ExceptionHandler(value = {RuntimeException.class})
@@ -37,5 +41,10 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiErrorDto("Internal server error :("));
     }
 
+    private HttpStatus getHttpStatus(Exception exception) {
+        return Optional.ofNullable(AnnotationUtils.findAnnotation(exception.getClass(), ResponseStatus.class))
+                .map(ResponseStatus::code)
+                .orElse(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
 
