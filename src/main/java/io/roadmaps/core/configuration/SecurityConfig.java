c659efecba91a730ff4c575a1de242f52007db4e
Dmitry Fedorov <d.b.fedorov@gmail.com>
2021-05-13 18:01:41 +0300

    Bugfix/cors4 (#13)
    * add web-mvc config for cors, disable cors on security config; update spring-parent and lombok
    
    * fix origin
    
    * refactor

--- src/main/java/io/roadmaps/core/configuration/SecurityConfig.java
+++ src/main/java/io/roadmaps/core/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.roadmaps.core.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -8,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,9 +19,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and()
+        http.cors().disable()
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().permitAll();
     }
+
 }
---
+++ src/main/java/io/roadmaps/core/configuration/Web.java
@@ -0,0 +1,17 @@
+package io.roadmaps.core.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Web implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns("http://localhost*");
+    }
+
+}
