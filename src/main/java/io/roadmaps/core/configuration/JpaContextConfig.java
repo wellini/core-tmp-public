530399f12cb5143a2564601520b4fa77cc444903
wellini <40068963+wellini@users.noreply.github.com>
2022-05-23 15:05:14 +0300

    Fix JPA (#26)
    Co-authored-by: Alexander <isanny314@gmail.com>

--- src/main/java/io/roadmaps/core/configuration/JpaContextConfig.java
+++ src/main/java/io/roadmaps/core/configuration/JpaContextConfig.java
@@ -1,23 +1,21 @@
 package io.roadmaps.core.configuration;
 
-import io.roadmaps.core.configuration.properties.JpaNativeProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.validation.Valid;
+import javax.sql.DataSource;
 
 @Configuration
 public class JpaContextConfig {
 
-    // Если не переопределить этот бин, ничего не работает ¯\_(ツ)_/¯
-    @Bean
-    public LocalEntityManagerFactoryBean entityManagerFactory(@Valid JpaNativeProperties jpaNativeProperties){
-        LocalEntityManagerFactoryBean localEntityManagerFactoryBean = new LocalEntityManagerFactoryBean();
-        localEntityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        localEntityManagerFactoryBean.setJpaProperties(jpaNativeProperties.getProperties());
-        return localEntityManagerFactoryBean;
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean containerEntityManagerFactoryBean(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        emf.setDataSource(dataSource);
+        return emf;
     }
 
 }
